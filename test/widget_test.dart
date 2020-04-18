5fdbe76fd66fa92f1d5ff3b8d606796120fa7152
adithya2000 <adithya.narayan2000@gmail.com>
2020-04-18 16:38:10 +0530

    Add files via upload

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bank_app/main.dart';
+import 'package:bankingsample/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
