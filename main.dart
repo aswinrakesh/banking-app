2f8af98f5f1188904c68ae99ed62771186a01e74
adithya2000 <adithya.narayan2000@gmail.com>
2020-04-20 22:27:22 +0530

    Loan module

---
+++ main.dart
@@ -0,0 +1,93 @@
+import 'dart:convert';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'screen2.dart';
+
+void main()=>runApp(MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Banking App',
+      home: Login(),
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  String username="",password="";
+  var dataJson;
+  TextEditingController user=TextEditingController();
+  TextEditingController pass=TextEditingController();
+
+  Future<String> getlogin(String user)async{
+    final uri = "10.0.2.2:4000";
+    final path = "/"+user;
+    final url= Uri.http(uri,path);
+    print(url);
+
+    Response response = await http.get(url,headers: {"Accept":"application/json"});
+    print(response.headers);
+    setState(() {
+      dataJson = json.decode(response.body);
+      print(dataJson);
+    });
+    return " Successful";
+  }
+
+  void verifylogin(String username,String password,var data){
+    //String name = data[0]['username'];
+    //String bal = data[0] ['balance'];
+    if(data[0]['password']==password){
+      Navigator.push(context,MaterialPageRoute(
+        builder: (BuildContext context) => MyApp1(text : data),
+      )
+      );
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: user,
+              decoration: InputDecoration(
+                hintText: 'Username',
+                icon: Icon(Icons.person),
+              ),
+            ),
+            TextField(
+              controller: pass,
+              decoration: InputDecoration(
+                  hintText: 'Password',
+                  icon: Icon(Icons.lock)
+              ),
+              autofocus: false,
+              obscureText: true,
+            ),
+            RaisedButton(
+              child: Text('SIGN IN'),
+              onPressed: (){
+                getlogin(user.text);
+                setState(() {
+                  verifylogin(user.text,pass.text,dataJson);
+                });
+
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
